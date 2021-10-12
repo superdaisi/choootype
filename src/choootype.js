403340d1bfa04531511acd0b12eeeabc6d9f4f5f
matsumura <y.matsumura@ejworks.com>
2021-10-12 17:18:06 +0900

    v 1.0

--- src/choootype.js
+++ src/choootype.js
@@ -13,6 +13,8 @@
  * @param {Object} spanWidth - spanの要素の幅
  * @param {Number} aspecgt - 親要素と子要素のの比率。長体の値
  *
+ * @todo 最小比率以下の場合文字が切れるので、なにかいい方法を考える。
+ *
  * Choootype 1.0
  *
  * Yoshiaki Matsumura
@@ -21,28 +23,35 @@
  *
  */
 var Choootype = function (sel = ".choootype", opt = {}) {
+  // init
   const minAspect = opt.min || 0;
   const tgt = document.querySelectorAll(sel);
+
   tgt.forEach((box) => {
+    // ターゲットのオブジェクトの中身にspanタグを追加
     let span = document.createElement("span");
     const boxWidth = box.offsetWidth;
+    const spanWidth = span.offsetWidth;
     span.innerHTML = box.innerHTML;
-    span.style.whiteSpace = "nowrap";
-
-    box.style.overflow = "hidden";
-
     box.innerHTML = "";
     box.appendChild(span);
 
-    const spanWidth = span.offsetWidth;
+    // 親要素の長さと文字部分の長さの比率を出す。<=文字の長体の比率
     console.log(boxWidth, spanWidth);
     let aspect = boxWidth / spanWidth;
 
+    // 文字が収まる場合はそのまま抜ける。
     if (aspect >= 1) {
       return;
+
+      // 文字の比率が最小値より小さい場合は最小値に設定する
+      // 可読性のため0.6倍くらいが望ましいが、固定してしまうと文字が切れてしまう。
     } else if (aspect < minAspect) {
       aspect = minAspect;
     }
+    // 親要素及び子要素にスタイルを適用する
+    box.style.overflow = "hidden";
+    span.style.whiteSpace = "nowrap";
     span.style.transform = "scale(" + aspect + ", 1)";
     span.style.display = "block";
     span.style.transformOrigin = "top left";
